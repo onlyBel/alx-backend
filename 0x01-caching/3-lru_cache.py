92d91618b5d9368a9b024dd344b7f0414babdfc1
onlyBel <belindawnjr@gmail.com>
2024-09-04 11:18:17 -0700

    cacheng

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/env python3
+""" caching system
+    """
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ caching system:
+    Args:
+        LRUCache ([class]): [basic caching]
+    """
+
+    def __init__(self) -> None:
+        """ initialize of class """
+        self.temp_list = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            self.temp_list.append(key)
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = self.temp_list.pop(0)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if (key is None) or not (key in self.cache_data):
+            return None
+        self.temp_list.insert(len(self.temp_list),
+                              self.temp_list.pop(self.temp_list.index(key)))
+        return self.cache_data.get(key)
